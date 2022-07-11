9612f8fd725e4bbc9e11cf4d46e8220b1d7e3a6c
Marcela Campo <mcampo@pivotal.io>
2022-07-11 16:57:46 +0100

    WIP

--- integration-tests/integration_test_suite_test.go
+++ integration-tests/integration_test_suite_test.go
@@ -23,7 +23,9 @@ var (
 	mockTerraform testframework.TerraformMock
 	broker        *testframework.TestInstance
 )
-
+// TODO credentials are hardcoded
+// TODO invocation vars include the command
+// TODO used the broker's terraform and not the system one
 var _ = BeforeSuite(func() {
 	var err error
 	mockTerraform, err = testframework.NewTerraformMock()
@@ -34,8 +36,8 @@ var _ = BeforeSuite(func() {
 
 	Expect(broker.Start(GinkgoWriter, []string{
 		"GSB_SERVICE_CSB_AWS_S3_BUCKET_PLANS=" + marshall(customS3Plans),
-		"AWS_ACCESS_KEY_ID=" + awsAccessKeyID,
-		"AWS_SECRET_ACCESS_KEY=" + awsSecretAccessKey,
+		"AWS_ACCESS_KEY_ID=",
+		"AWS_SECRET_ACCESS_KEY=",
 		"CSB_LISTENER_HOST=localhost",
 		"GSB_COMPATIBILITY_ENABLE_BETA_SERVICES=true",
 	})).To(Succeed())
--- terraform/s3/provision/main.tf
+++ terraform/s3/provision/main.tf
@@ -14,11 +14,8 @@
 
 resource "aws_s3_bucket" "b" {
   bucket = var.bucket_name
-  acl    = var.acl
 
-  versioning {
-    enabled = var.enable_versioning
-  }
+
 
   tags = var.labels
 
